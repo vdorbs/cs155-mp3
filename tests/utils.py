ffcc96d4a2705972897a99600d3e3e71b5bbc1c7
Kristján Eldjárn <kristjan@eldjarn.net>
2018-03-03 16:33:25 -0800

    Added append_space() to tests/utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,9 @@
 import itertools
+
+def append_space(syllable_list):
+    syllable_list[-1] += ' '
+    return syllable_list
+
 def split_line_by_syllable(line, pyphen):
-    return list(itertools.chain(*[pyphen(word).split('-') for word in line]))
+    return list(itertools.chain(*[append_space(pyphen(word).split('-')) for word in line]))
+
