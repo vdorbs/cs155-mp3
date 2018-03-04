567aefb2c6b0c4dddc42a99e3f7e4dc7ffb85e2a
Victor Dorobantu <victor.d.dorobantu@gmail.com>
2018-03-03 18:06:49 -0800

    Added Test class, MultinomialHMM test case

--- tests/preprocess.py
+++ tests/preprocess.py
@@ -14,3 +14,9 @@ def split_by_syllables(data):
     syl = pyphen.Pyphen(lang='en')
     return [[split_line_by_syllable(line, syl.inserted) for line in sonnet] for sonnet in data]
 
+def tokenize(data):
+    token_dictionary = create_token_dictionary(data)
+    return [[[token_dictionary[elem] for elem in line] for line in sonnet] for sonnet in data]
+
+def flatten_sonnets(data):
+    return list(itertools.chain(*data))
--- tests/utils.py
+++ tests/utils.py
@@ -7,3 +7,13 @@ def append_space(syllable_list):
 def split_line_by_syllable(line, pyphen):
     return list(itertools.chain(*[append_space(pyphen(word).split('-')) for word in line]))
 
+def create_token_dictionary(data):
+    unique_elems = set()
+    for sonnet in data:
+        for line in sonnet:
+            for elem in line:
+                unique_elems.add(elem)
+    return {elem:token for token, elem in enumerate(unique_elems)}
+
+def get_sequence_lengths(data):
+    return [len(line) for line in data]
