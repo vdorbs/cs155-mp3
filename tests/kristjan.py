e56fbea3c2b1253183cad9e9d93d84595389fab4
Kristján Eldjárn <kristjan@eldjarn.net>
2018-03-03 16:20:48 -0800

    Added a data processing test

--- tests/kristjan.py
+++ tests/kristjan.py
@@ -1,3 +1,11 @@
 from .test_sweet import TestSweet
+from .preprocess import (
+        split_by_words,
+        split_by_syllables
+)
 
 ts = TestSweet()
+ts.add_pipeline([
+    split_by_words,
+    split_by_syllables
+])
