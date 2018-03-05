6d023d3fe3f7dad53c7d704357ede9f8571ef882
Bijan Mazaheri <bijanmazaheri@Bijans-MacBook-Pro.local>
2018-03-05 15:03:04 -0800

    predict not working

--- tests/bijan.py
+++ tests/bijan.py
@@ -30,13 +30,22 @@ def fit(data):
     print(x.shape)
     print(y.shape)
     y = np_utils.to_categorical(y)
-    model.add(LSTM(units = 150, input_shape = (40, 1)))
+    model.add(LSTM(units = 100, input_shape = (40, 1)))
     model.add(Dense(token_size + 1, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
-    model.fit(x, y, epochs=10, verbose=1)
+    model.fit(x, y, epochs=1, verbose=1)
 
 def run(token_to_elem):
-    return []
+    seed = "shall i compare thee to a summer's day?\n"
+    elem_to_token = {v:k for k, v in token_to_elem.items()}
+    poem_char_indices = [elem_to_token[char] for char in seed]
+    for line in range(9):
+        next_char = ""
+        while(next_char != "\n"):
+            x = [np.array([[char] for char in poem_char_indices[-40:]])]
+            next_char = model.predict(x)
+            poem_char_indices.append(next_char)
+    return "".join([elem_to_token(tok) for tok in poem_char_indices])
 
 ts.add_test(Test(model,fit, run), idx)
 
